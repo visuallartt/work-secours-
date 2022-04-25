c4c12f62bb2abd73eba99aa4d347b51e8d875223
visuallartt <97293252+visuallartt@users.noreply.github.com>
2022-04-25 23:24:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function(){
+			$(".card").mouseover(function(){
+				$(this).css("");
+			})
+			
+		})
+window.addEventListener('scroll', function(){
+	console.log("je srolle");
+});*/
+
+AOS.init({
+	duration:1000,
+})
---
+++ php/cible.php
@@ -0,0 +1,3 @@
+<?
+echo"DEMARRER LES SERVEURS";
+?>
